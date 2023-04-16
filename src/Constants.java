72b577d96063fa3fe0c51fc685460a679a55ea4a
Mishka <michesc33@gmail.com>
2023-04-17 00:07:20 +0300

    Initial commit

---
+++ src/Constants.java
@@ -0,0 +1,33 @@
+public class Constants {
+
+    public static final int GAME_WINDOW_WIDTH = 800;
+    public static final int GAME_WINDOW_HEIGHT = 500;
+
+    public static final int CONTROL_PANEL_WIDTH = 150;
+
+    public static final int MARGIN_BUTTON = 15;
+
+    public static final int MARGIN_LABEL = 5;
+
+    public static final int INSTRUCTION_BUTTON_WIDTH = 150;
+
+    public static final int BUTTON_HEIGHT = 50;
+
+    public static final int PLAY_BUTTON_WIDTH = 100;
+
+    public static final int INSTRUCTION_WINDOW_WIDTH = 550;
+
+    public static final int INSTRUCTION_WINDOW_HEIGHT = 350;
+
+    public static final int Y_START = 0;
+
+    public static final int X_START = 0;
+
+    public static final int LABEL_HEIGHT = 30;
+
+    public static final int LABEL_SIZE = 16;
+
+    public static final int BUTTON_BORDERS_THICKNESS = 3;
+
+    public static final int BUTTON_FONT_SIZE = 20;
+}
